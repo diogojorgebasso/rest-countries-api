aa693d4f55efbc9e9ad12ea6662092d45db623ed
Diogo Basso <diogojbasso@gmail.com>
2021-01-11 15:11:03 -0300

    fixed form in search

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Search.css";
+
 export default function Search() {
+  function filterContinent() {
+    //pass to App.js filter
+  }
   return (
     <div className="Search">
       <input type="text" />
-      <form action="searchContinent">
+      <form>
         <label class="src-only" for="continents">
           Choose a continent to filter:
         </label>
